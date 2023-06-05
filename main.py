4a9f6c065d06cdedb19ee64656b701dfef0c308b
GalYona21 <63791604+GalYona21@users.noreply.github.com>
2023-06-05 23:12:36 +0300

    Calculated all the needed derivatives of the principal curvatures in the codazzi equation using finite derivatives. Also added a plot function.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyvista
 import torch
 from torch_geometric.nn import fps
 
-from utils import rearange_mesh_faces, calc_dk1_1
+from utils import rearange_mesh_faces, calc_dki_j, plot_mesh_and_color_by_k_or_dki_j
 
 
 def main():
@@ -14,12 +14,15 @@ def main():
     points = numpy.stack([grid_x.ravel(), grid_y.ravel(), numpy.zeros(shape=grid_points_count*grid_points_count)], axis=1)
 
     points_tensor = torch.tensor(points)
-    indices = fps(x=points_tensor, ratio=0.1)
-    sampled_points = points_tensor[indices]
-    sampled_points = sampled_points.cpu().detach().numpy()
-    sampled_points[:, 2] = 0.1*numpy.sin(2*numpy.pi*sampled_points[:, 0]*0.5) + numpy.cos(4*numpy.pi*sampled_points[:, 1]*0.1)
 
-    cloud = pyvista.PolyData(sampled_points)
+    # make the patch more irregular and more real world like
+    # indices = fps(x=points_tensor, ratio=0.1)
+    # sampled_points = points_tensor[indices]
+    # sampled_points = sampled_points.cpu().detach().numpy()
+    # sampled_points[:, 2] = 0.1*numpy.sin(2*numpy.pi*sampled_points[:, 0]*0.5) + numpy.cos(4*numpy.pi*sampled_points[:, 1]*0.1)
+    points_tensor = points_tensor.cpu().detach().numpy()
+    points_tensor[:, 2] = 0.1*numpy.sin(2*numpy.pi*points_tensor[:, 0]*0.5) + numpy.cos(4*numpy.pi*points_tensor[:, 1]*0.1)
+    cloud = pyvista.PolyData(points_tensor)
     # cloud.plot(point_size=5)
 
     surf = cloud.delaunay_2d()
@@ -32,9 +35,22 @@ def main():
 
 
     v1, v2, k1, k2 = igl.principal_curvature(v, f)
+    plot_mesh_and_color_by_k_or_dki_j(surf, k1, "k1")
+    plot_mesh_and_color_by_k_or_dki_j(surf, k2, "k2")
+    delta = (1/grid_points_count) * 20
+    dk1_1 = calc_dki_j(v, v1, k1, delta)
+    # plot_mesh_and_color_by_k_or_dki_j(surf, dk1_1, "dk1_1")
 
-    dk1_1 = calc_dk1_1(v, v1, k1)
-
+    dk1_2 = calc_dki_j(v, v2, k1, delta)
+    plot_mesh_and_color_by_k_or_dki_j(surf, dk1_2, "dk1_2")
+    dk2_1 = calc_dki_j(v, v1, k2, delta)
+    plot_mesh_and_color_by_k_or_dki_j(surf, dk2_1, "dk2_1")
+    dk2_2 = calc_dki_j(v, v2, k2, delta)
+    plot_mesh_and_color_by_k_or_dki_j(surf, dk2_2, "dk2_2")
+    dk1_22 = calc_dki_j(v, v2, dk1_2, delta)
+    plot_mesh_and_color_by_k_or_dki_j(surf, dk1_22, "dk1_22")
+    dk2_11 = calc_dki_j(v, v1, dk2_1, delta)
+    plot_mesh_and_color_by_k_or_dki_j(surf, dk2_11, "dk2_11")
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pyvista import PolyData
 from sklearn.neighbors import KDTree
 
 
@@ -7,15 +8,22 @@ def rearange_mesh_faces(faces):
     faces = faces.reshape(-1, 3)
     return faces
 
-def calc_dk1_1(points, v1, k1):
-    delta = 0.1
+def calc_dki_j(points, desired_principal_dir, k_i, delta):
+    """
+    calculate derivative of k_i in the direction of desired_principal_dir
+    :param points:
+    :param desired_principal_dir:
+    :param k_i:
+    :param delta:
+    :return:
+    """
     output = np.zeros((len(points), 1))
     A = KDTree(points)
     for i in range(len(points)):
         p = points[i]
-        p_delta = p + delta * v1[i]
+        p_delta = p + delta * desired_principal_dir[i]
         p_delta = p_delta[np.newaxis, :]
-        p_minus_delta = p - delta * v1[i]
+        p_minus_delta = p - delta * desired_principal_dir[i]
         p_minus_delta = p_minus_delta[np.newaxis, :]
         # calculate closest point to p_delta using kd-tree
         closest_point_p_delta = A.query(p_delta,k=2)
@@ -35,6 +43,9 @@ def calc_dk1_1(points, v1, k1):
 
 
 
-        output[i] = (k1[p_delta] - k1[p_minus_delta]) / (np.linalg.norm(points[p_delta] - points[p_minus_delta]))
+        output[i] = (k_i[p_delta] - k_i[p_minus_delta]) / (np.linalg.norm(points[p_delta] - points[p_minus_delta]))
 
     return output
+
+def plot_mesh_and_color_by_k_or_dki_j(surf : PolyData, k, title):
+    surf.plot(scalars=k, show_edges=True, text=title, cmap='jet', cpos='xy', screenshot='k.png')
