2b52fc4f1b049d577fd6fea06da3314afefcf9fe
GalYona21 <63791604+GalYona21@users.noreply.github.com>
2023-06-09 11:34:07 +0300

    added function that calculate the derivatives and the pearson correlation matrix

--- main.py
+++ main.py
@@ -4,35 +4,20 @@ import pyvista
 import torch
 from torch_geometric.nn import fps
 
-from utils import rearange_mesh_faces, calc_dki_j, plot_mesh_and_color_by_k_or_dki_j, plot_mesh_with_vector_field, generate_surface
+import dataset_generator
+from utils import rearange_mesh_faces, calc_dki_j, plot_mesh_and_color_by_k_or_dki_j, plot_mesh_with_vector_field, generate_surface, calculate_pearson_corr_matrix
 
 
 def main():
     grid_points_count = 200
-    grid_linspace = numpy.linspace(-1, 1, grid_points_count)
-    grid_x, grid_y = numpy.meshgrid(grid_linspace, grid_linspace)
-    points = numpy.stack([grid_x.ravel(), grid_y.ravel(), numpy.zeros(shape=grid_points_count*grid_points_count)], axis=1)
+    surf, v, f =generate_surface(grid_points_count)
 
+    delta = (1/grid_points_count) * 20
 
-    points_tensor = torch.tensor(points)
+    calculate_pearson_corr_matrix(v, f, delta=delta)
 
-    # make the patch more irregular and more real world like
-    # indices = fps(x=points_tensor, ratio=0.1)
-    # sampled_points = points_tensor[indices]
-    # sampled_points = sampled_points.cpu().detach().numpy()
-    # sampled_points[:, 2] = 0.1*numpy.sin(2*numpy.pi*sampled_points[:, 0]*0.5) + numpy.cos(4*numpy.pi*sampled_points[:, 1]*0.1)
-    points_tensor = points_tensor.cpu().detach().numpy()
-    points_tensor[:, 2] = 0.1*numpy.sin(2*numpy.pi*points_tensor[:, 0]*0.5) + numpy.cos(4*numpy.pi*points_tensor[:, 1]*0.1)
-    cloud = pyvista.PolyData(points_tensor)
-    # cloud.plot(point_size=5)
-
-    surf = cloud.delaunay_2d()
-    # surf.plot(show_edges=True)
-
-    v = surf.points
-    f = surf.faces
-    f = rearange_mesh_faces(f)
     # format the faces in the right format
+    surf.plot( show_edges=False, cmap='jet', cpos='xy', screenshot='k.png')
 
     # generate less dense surface for the vector field visualization
     surf_vec_field, v_surf_vec_field, f_surf_vec_field = generate_surface(30)
@@ -40,23 +25,35 @@ def main():
     v1, v2, k1, k2 = igl.principal_curvature(v, f)
     v_less_dense1, v_less_dense2, k_less_dense1, k_less_dense2 = igl.principal_curvature(v_surf_vec_field, f_surf_vec_field)
 
-    plot_mesh_with_vector_field(surf, surf_vec_field, k1,v_less_dense1 , v_less_dense2)
-    plot_mesh_and_color_by_k_or_dki_j(surf, k1, "k1")
-    plot_mesh_and_color_by_k_or_dki_j(surf, k2, "k2")
-    delta = (1/grid_points_count) * 20
+    plot_mesh_with_vector_field(surf, surf_vec_field, k1,v_less_dense1 , v_less_dense2, title="k1")
+    plot_mesh_with_vector_field(surf, surf_vec_field, k2,v_less_dense1 , v_less_dense2, title="k2")
+
+    # plot_mesh_and_color_by_k_or_dki_j(surf, k1, "k1")
+    # plot_mesh_and_color_by_k_or_dki_j(surf, k2, "k2")
     dk1_1 = calc_dki_j(v, v1, k1, delta)
     # plot_mesh_and_color_by_k_or_dki_j(surf, dk1_1, "dk1_1")
+    plot_mesh_with_vector_field(surf, surf_vec_field, dk1_1,v_less_dense1 , v_less_dense2, title="dk1_1")
 
     dk1_2 = calc_dki_j(v, v2, k1, delta)
-    plot_mesh_and_color_by_k_or_dki_j(surf, dk1_2, "dk1_2")
+    plot_mesh_with_vector_field(surf, surf_vec_field, k2,v_less_dense1 , v_less_dense2, title="dk1_2")
+
+    # plot_mesh_and_color_by_k_or_dki_j(surf, dk1_2, "dk1_2")
     dk2_1 = calc_dki_j(v, v1, k2, delta)
-    plot_mesh_and_color_by_k_or_dki_j(surf, dk2_1, "dk2_1")
+    plot_mesh_with_vector_field(surf, surf_vec_field, k2,v_less_dense1 , v_less_dense2, title="dk2_1")
+
+    # plot_mesh_and_color_by_k_or_dki_j(surf, dk2_1, "dk2_1")
     dk2_2 = calc_dki_j(v, v2, k2, delta)
-    plot_mesh_and_color_by_k_or_dki_j(surf, dk2_2, "dk2_2")
+    plot_mesh_with_vector_field(surf, surf_vec_field, k2,v_less_dense1 , v_less_dense2, title="dk2_2")
+
+    # plot_mesh_and_color_by_k_or_dki_j(surf, dk2_2, "dk2_2")
     dk1_22 = calc_dki_j(v, v2, dk1_2, delta)
-    plot_mesh_and_color_by_k_or_dki_j(surf, dk1_22, "dk1_22")
+    plot_mesh_with_vector_field(surf, surf_vec_field, k2,v_less_dense1 , v_less_dense2, title="dk1_22")
+
+    # plot_mesh_and_color_by_k_or_dki_j(surf, dk1_22, "dk1_22")
     dk2_11 = calc_dki_j(v, v1, dk2_1, delta)
-    plot_mesh_and_color_by_k_or_dki_j(surf, dk2_11, "dk2_11")
+    plot_mesh_with_vector_field(surf, surf_vec_field, k2,v_less_dense1 , v_less_dense2, title="dk2_11")
+
+    # plot_mesh_and_color_by_k_or_dki_j(surf, dk2_11, "dk2_11")
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import igl
 import numpy as np
 import torch
 from pyvista import PolyData
@@ -52,9 +53,10 @@ def calc_dki_j(points, desired_principal_dir, k_i, delta):
 
 def plot_mesh_and_color_by_k_or_dki_j(surf : PolyData, k, title):
     surf.plot(scalars=k, show_edges=False, text=title, cmap='jet', cpos='xy', screenshot='k.png')
-#     how to plot also the surf vectors
 
-def plot_mesh_with_vector_field(surf : PolyData,visualization_surf, k,  principal_directions1, principal_directions2):
+
+
+def plot_mesh_with_vector_field(surf : PolyData,visualization_surf : PolyData, k,  principal_directions1, principal_directions2,title):
     plotter = pv.Plotter()
     visualization_surf['principal_directions1'] = principal_directions1
     visualization_surf['principal_directions2'] = principal_directions2
@@ -64,6 +66,7 @@ def plot_mesh_with_vector_field(surf : PolyData,visualization_surf, k,  principa
 
     plotter.add_mesh(glyphs1, color='blue', opacity=1)
     plotter.add_mesh(glyphs2, color='red', opacity=1)
+    plotter.add_title(title=title)
     plotter.show(screenshot='k.png')
 
 
@@ -83,8 +86,13 @@ def generate_surface(grid_points=200):
     # sampled_points = sampled_points.cpu().detach().numpy()
     # sampled_points[:, 2] = 0.1*numpy.sin(2*numpy.pi*sampled_points[:, 0]*0.5) + numpy.cos(4*numpy.pi*sampled_points[:, 1]*0.1)
     points_tensor = points_tensor.cpu().detach().numpy()
+    # scalar rand between 0 to 100
+    a = np.random.rand()*2
+    b = np.random.rand()*2
     points_tensor[:, 2] = 0.1 * np.sin(2 * np.pi * points_tensor[:, 0] * 0.5) + np.cos(
         4 * np.pi * points_tensor[:, 1] * 0.1)
+    # points_tensor[:, 2] = points_tensor[:, 0]**2 + points_tensor[:, 1]**2
+
     cloud = pv.PolyData(points_tensor)
     # cloud.plot(point_size=5)
 
@@ -95,3 +103,27 @@ def generate_surface(grid_points=200):
     f = surf.faces
     f = rearange_mesh_faces(f)
     return surf, v,f
+
+
+def calculate_pearson_corr_matrix(v, f, delta=0.01):
+    v1, v2, k1, k2 = igl.principal_curvature(v, f)
+
+
+    # plot_mesh_and_color_by_k_or_dki_j(surf, k1, "k1")
+    # plot_mesh_and_color_by_k_or_dki_j(surf, k2, "k2")
+    dk1_1 = calc_dki_j(v, v1, k1, delta)
+
+    dk1_2 = calc_dki_j(v, v2, k1, delta)
+
+    dk2_1 = calc_dki_j(v, v1, k2, delta)
+
+    dk2_2 = calc_dki_j(v, v2, k2, delta)
+
+    dk1_22 = calc_dki_j(v, v2, dk1_2, delta)
+
+    dk2_11 = calc_dki_j(v, v1, dk2_1, delta)
+
+#     calculate pearson correlation matrix
+    pearson_corr_matrix = np.corrcoef(np.hstack([k1[:,np.newaxis],k2[:,np.newaxis],dk1_1, dk1_2, dk2_1, dk2_2, dk1_22, dk2_11]),rowvar=False)
+
+    return pearson_corr_matrix
