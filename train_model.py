566979a9e3ea48dd7b31989564d8c874cdd76494
GalYona21 <63791604+GalYona21@users.noreply.github.com>
2024-01-01 13:49:48 +0200

    Changed training to 9 features which are x,y,z,xx,xy,xz,yy,yz,zz. Added normalization to each patch in training and evaluation.

--- train_model.py
+++ train_model.py
@@ -46,16 +46,16 @@ def main_loop():
     file_path2 = ''
     file_path3 = ''
     if combine_reg_and_non_reg_patches:
-        file_path2 = server_dir + "triplets_data_size_50_N_10000_all_monge_patch_normalized_pos_and_rot.pkl"
-        with open(file_path2, 'rb') as f:
-            data2 = pickle.load(f)
+        # file_path2 = server_dir + "triplets_data_size_50_N_10000_all_monge_patch_normalized_pos_and_rot.pkl"
+        # with open(file_path2, 'rb') as f:
+        #     data2 = pickle.load(f)
         file_path3 = server_dir + "triplets_dataset/triplets_size_300_N_1000_all_monge_patch_non_uniform_sampling_with_parabolic_patches.pkl"
         with open(file_path3, 'rb') as f:
             data3 = pickle.load(f)
 
 
 
-        data = data + data2 + data3
+        data = data + data3
 
     # Create custom dataset
     custom_dataset = CustomTripletDataset(data)
