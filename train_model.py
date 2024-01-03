47d7033fc77bdf88405c00766e928d16c2b3290d
GalYona21 <63791604+GalYona21@users.noreply.github.com>
2024-01-03 17:04:04 +0200

    Modified dataset generation to create non-triplets patch datasets

--- train_model.py
+++ train_model.py
@@ -31,7 +31,7 @@ def main_loop():
         # file_path = server_dir + "triplets_dataset/" + data_file_name
         file_path = server_dir+"data/spherical_monge_patches_100_N_20000.pkl"
         file_path2 = server_dir+"data/hyperbolic_monge_patches_100_N_20000.pkl"
-        file_path3 = server_dir+"data/planar_monge_patches_100_N_20000.pkl"
+        file_path3 = server_dir+"data/parabolic_monge_patches_100_N_20000.pkl"
         num_workers = 1
         # combine_reg_and_non_reg_patches = True
 
