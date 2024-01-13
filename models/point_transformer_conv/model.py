14f2235b8a71c2168ffb3a88b20c3f8272dfbee7
GalYona21 <63791604+GalYona21@users.noreply.github.com>
2024-01-13 15:12:55 +0200

    Added analytical mapping to patches to 2d Changed loss function to just contrastive to check it

--- models/point_transformer_conv/model.py
+++ models/point_transformer_conv/model.py
@@ -127,8 +127,8 @@ class PointTransformerConvNet(pl.LightningModule):
 
     def forward(self, data, global_pooling=True):
 
-        x = self.append_moments(data.x) # x,y,z,xx,xy,xz,yy,yz,zz coordinates of each point
-        # x = data.x # x,y,z coordinates of each point
+        # x = self.append_moments(data.x) # x,y,z,xx,xy,xz,yy,yz,zz coordinates of each point
+        x = data.x # x,y,z coordinates of each point
         # edge_index = radius_graph(x, r=0.5, batch=None, loop=True, max_num_neighbors=32)
 
         # Apply initial embedding
